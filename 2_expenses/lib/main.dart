33a5d212613418cc977fdda923542809bf91c553
Diego <dipeoliver@outlook.com>
2021-05-10 22:38:26 -0300

    update class 91/ Date format

--- 2_expenses/lib/main.dart
+++ 2_expenses/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
+// trabalhar com formatação de data e ano
+import 'package:intl/intl.dart';
+
 main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
@@ -16,13 +19,13 @@ class MyHomePage extends StatelessWidget {
   final _transactions = [
     Transactions(
         id: 't1',
-        title: 'novo tenis de corrida',
+        title: 'Novo tenis de corrida',
         value: 310.76,
         date: DateTime.now()),
     Transactions(
       id: 't2',
       title: 'Bermuda',
-      value: 58.10,
+      value: 158.10,
       date: DateTime.now(),
     ),
   ];
@@ -34,10 +37,12 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
+            width: 20,
+            height: 100,
             //width: double.infinity,
             child: Card(
               child: Text('Gráfico'),
@@ -49,11 +54,41 @@ class MyHomePage extends StatelessWidget {
             // converter ums transação (objetos) em  itens visual
             children: _transactions.map((tr) {
               return Card(
-                child: Text(tr.title),
+                child: Row(
+                  children: [
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                       'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple),
+                      ),
+                    ),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tr.title,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            // formatação com bilbioteca
+                            DateFormat('d MMM y').format(tr.date),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ])
+                  ],
+                ),
               );
             }).toList(),
-            //------------------------------------
-          )
+          ),
         ],
       ),
     );
